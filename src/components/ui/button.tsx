4a810b017ecd992b310ad90b7d35601e73fa5fee
gus <gusquinalha@gmail.com>
2026-05-11 13:47:58 -0300

    feat: add pages, documentation and UI improvements

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
       size: {
         xs: "h-8 px-3 text-xs",
         default: "h-10 px-4 text-base",
-        sm: "h-9 text-sm",
+        sm: "h-9 px-3 text-sm",
         lg: "h-11 px-8 text-lg",
         icon: "h-10 w-10",
         "icon-sm": "h-9 w-9",
